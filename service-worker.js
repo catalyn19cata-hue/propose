7bbc9dd1da46719e24e0dd57d740c77b5a629858
catalyn19cata-hue <catalyn19cata@gmail.com>
2026-03-17 23:20:14 +0100

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,21 @@
+const cacheName = 'cc-app-v1';
+const filesToCache = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/Fer1.jpeg',
+  '/muzica.mpeg',
+  // adaugă toate fișierele importante
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(cacheName).then(cache => cache.addAll(filesToCache))
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(response => response || fetch(e.request))
+  );
+});
